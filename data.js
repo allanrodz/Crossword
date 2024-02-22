1c889dae5791916a8d3df4d607f2fb10f46180a8
Francisco Lairton Rodolfo Calaca (ALLAN) <75245185+allanrodz@users.noreply.github.com>
2024-02-22 23:55:27 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+CrosswordWidth = 14;
+CrosswordHeight = 17;
+Words = 11;
+WordLength = new Array(8, 5, 11, 6, 6, 4, 5, 4, 9, 6, 8);
+Word = new Array("HENNESSY", "KEHER", "FITZPATRICK", "LARKIN", "CLEERE", "CODY", "WALSH", "REID", "HEFFERNAN", "SKEHAN", "FENNELLY");
+Clue = new Array("Known for his versatility on the field and a unique grip on the camán.", 
+    "A legendary player who scored a record in a final, yet ended on the losing side.", 
+    "A player who could turn on a sixpence, known for his tight yet fluent style of play.", 
+    "Epitomized determination and grit, overcoming physical limitations to excel in defense.", 
+    "A wing-back known for his elegance and style, his career was cut short by a knee injury.", 
+    "Guided Kilkenny to multiple triumphs, including a notable \"double-double\" victory series.", 
+    "Legendary goalkeeper and later manager, known for his exceptional leadership both on and off the field.", 
+    "A current player with exceptional under-age promise, known for his control and deft touch.", 
+    "His two goals in a forty-second spell were pivotal in securing a victory over Cork in a final.", 
+    "The first Kilkenny player to win nine All-Ireland medals, a goalie known for his innovation.", 
+    "A quiet yet impactful player from Ballyhale Shamrocks, known for his silent but deadly play.");
+AnswerHash = new Array(10062, 15240, 64664, 51961, 28034, 32251, 37825, 17218, 79631, 27025, 27165);
+WordX = new Array(4, 7, 3, 8, 2, 0, 10, 5, 8, 13, 3);
+WordY = new Array(3, 6, 9, 13, 14, 16, 0, 2, 2, 8, 9);
+LastHorizontalWord = 5;
+OnlyCheckOnce = false;
+
